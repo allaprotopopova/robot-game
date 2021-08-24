0825287662660d05e732ec40ad4b52534849e10d
Alla Protopopova <Alla.Protopopova@excelian.com>
2021-08-24 10:06:29 +0300

    refactoring and Main class test

--- src/test/java/ru/protopopova/MainTest.java
+++ src/test/java/ru/protopopova/MainTest.java
@@ -35,45 +35,43 @@ class MainTest {
 
     @Test
     void normalFlow() throws IOException {
-        input("""
-                PLACE 1,2,EAST
-                REPORT
-                MOVE
-                MOVE
-                LEFT
-                MOVE
-                REPORT
-                EXIT""");
+        input("PLACE 1,2,EAST\n" +
+                "REPORT\n" +
+                "MOVE\n" +
+                "MOVE\n" +
+                "LEFT\n" +
+                "MOVE\n" +
+                "REPORT\n" +
+                "EXIT");
         Main.main(new String[0]);
+
         assertEquals("1,2,EAST\n" +
-                "3,3,NORTH", outputStreamCaptor.toString().trim());
+                "3,3,NORTH", outputStreamCaptor.toString().replaceAll("\r", "").trim());
     }
 
     @Test
     void ignoreCommandsBeforePlace() throws IOException {
-        input("""
-                MOVE
-                LEFT
-                RIGHT
-                REPORT
-                PLACE 1,1,WEST
-                REPORT
-                EXIT""");
+        input("MOVE\n" +
+                "LEFT\n" +
+                "RIGHT\n" +
+                "REPORT\n" +
+        "PLACE 1,1,WEST\n" +
+                "REPORT\n" +
+                "EXIT");
         Main.main(new String[0]);
         assertEquals("1,1,WEST", outputStreamCaptor.toString().trim());
     }
 
     @Test
     void preventInvalidPlacing() throws IOException {
-        input("""
-                PLACE 4,4,NORTH
-                REPORT
-                PLACE 6,8,NORTH
-                REPORT
-                EXIT""");
+        input("PLACE 4,4,NORTH\n" +
+                "REPORT\n" +
+                "PLACE 6,8,NORTH\n" +
+                "REPORT\n" +
+                "EXIT");
         Main.main(new String[0]);
         assertEquals("4,4,NORTH\n" +
-                "4,4,NORTH", outputStreamCaptor.toString().trim());
+                "4,4,NORTH", outputStreamCaptor.toString().replaceAll("\r", "").trim());
     }
 
 
